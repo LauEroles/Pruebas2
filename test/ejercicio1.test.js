616e00ed7079fcd236ad1bc93eab8a584f8d984f
Laura Eroles <laura.eroles@gmail.com>
2022-06-03 13:23:30 -0300

    cambios ejercicio1 y ejercicio1 test

--- test/ejercicio1.test.js
+++ test/ejercicio1.test.js
@@ -2,12 +2,13 @@
 // calculo de el area de un circulo que reciba el radio
 //Área = radio X radio X 3,14
 
+
 const ejercicio1 = require('../src/ejercicio1');
 
 describe('ejercicio1 expect', () => {
 
     it ('Calculates the circle area', ()=> {
-        const result= ejercicio1.areaCirculo(5,3.14);
-        expect (result).toBe("78.5");
+        const result= ejercicio1.areaCirculo(5);
+        expect (result).toBe(78.5);
         })
 });
