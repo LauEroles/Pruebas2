3e89a01d53526744901b958fd51eba966554fd50
Laura Eroles <laura.eroles@gmail.com>
2022-06-03 13:02:04 -0300

    Cambios branch ejercicio1

--- test/ejercicio1.test.js
+++ test/ejercicio1.test.js
@@ -1,3 +1,13 @@
-describe('ejercicio1 expected', ()=>{
-    
-})
+//// requerimiento 
+// calculo de el area de un circulo que reciba el radio
+//Área = radio X radio X 3,14
+
+const ejercicio1 = require('../src/ejercicio1');
+
+describe('ejercicio1 expect', () => {
+
+    it ('Calculates the circle area', ()=> {
+        const result= ejercicio1.areaCirculo(5,3.14);
+        expect (result).toBe("78.5");
+        })
+});
